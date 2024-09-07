58654051d634340a45de608b95bbf554599167ba
Haris <mohammadharis7871@gmail.com>
2024-09-08 01:18:25 +0530

    change resume

--- app.js
+++ app.js
@@ -64,13 +64,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 const photos = [
-  // "assets/haris.jpeg",
-  // "assets/haris2.jpg",
   "assets/haris3.jpg",
-  // "assets/haris4.jpg",
-  // "assets/haris5.jpg",
-  // "assets/haris6.jpg",
-  // "assets/haris7.jpg",
 ];
 
 function getRandPic() {
